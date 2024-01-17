1bb41d02565671d5c109c8e0010d514b73ba9be5
Osheen Judith Dcosta <137905130+judy026@users.noreply.github.com>
2024-01-17 21:05:43 +0530

    updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
-            <Link className="text-white font-bold" href={"/"}>GTCoding.</Link>
+        <nav className="flex justify-between items-center bg-slate-600 px-8 py-3">
+            <Link className="text-white font-bold" href={"/"}>NoobSiting.</Link>
             <Link className="bg-white p-2" href={'/addTopic'}>Add Topic</Link>
         </nav>
     )
